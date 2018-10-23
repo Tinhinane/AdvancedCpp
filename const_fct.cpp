844f554e00560a009cd9a4a6e2b6e3a2eba780eb
Tinhinane <tinhinane.aithamouda@gmail.com>
2018-10-23 16:38:10 +0200

    add comments about changing data members in const functions

--- const_fct.cpp
+++ const_fct.cpp
@@ -23,11 +23,19 @@ class Student{
     //add the keyword "const" at the end of a funct declaration to make it a const function
     //a const function can only call another const function
     void print_student() const{
+        //changing data members in const functions is illegal
+        //because "this" pointer type is "const Student* const"
+        //meaning it is const pointer pointing to a const value
+        //_id = 2000;
         cout << "const fct" << endl;
         cout << "Student id is : " << _id << endl;
     }
     //overloading the function with a non-const version
     void print_student() {
+        //changing data members is legal
+        //because "this" pointer type in non-const member fcts is "Student* const"
+        //meaning we can change what it points to, but we cannot change the location memory it points to
+        _id = 2000;
         cout << "non-const fct" << endl;
         cout << "Student id is : " << get_id() << endl;
     }
