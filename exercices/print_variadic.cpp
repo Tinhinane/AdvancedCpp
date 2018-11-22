d4673c9c03b5ab8464363396914ac3a5fc4d8a76
Tinhinane <tinhinane.aithamouda@gmail.com>
2018-11-22 16:56:43 +0100

    add a solution based on fold expressions

--- exercices/print_variadic.cpp
+++ exercices/print_variadic.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <sstream>
 using namespace std;
 
 struct X { };
@@ -9,33 +9,65 @@ ostream& operator<<(ostream& os, X const&)
   return os << "X";
 }
 
-//The base case: to produce an empty line when zero argument passed
-void print() {
-    cout << "\n";
-}
+namespace method_a{
+    //The base case: to produce an empty line when zero argument passed
+    void print() {
+        cout << "\n";
+    }
 
-// The recursive case: we take an argument, alongside
-// some other args, and print them
-template <typename T, typename...Args>
-void print(T arg, Args...args){
-    cout << arg << " ";
-    print(args...);
-}
+    // The recursive case: we take an argument, alongside
+    // some other args, and print them
+    template <typename T, typename...Args>
+    void print(T arg, Args...args){
+        cout << arg << " ";
+        print(args...);
+    }
+};
+//fold expressions
+namespace method_fe{
+    /*
+    C++17
+    Fold expressions can expand using any operator including the comma-operator
+
+    Assume that we call:
+    print(1, 2, 3)
+
+    Then this fold will expand to:
+
+    (cout << 1 << " "), (cout << 2 << " "), (cout << 3 << " "), (cout << '\n')
+
+    which is equivalent to:
+
+    cout << 1 << " ";
+    cout << 2 << " ";
+    cout << 3 << " ";
+    cout << '\n';
+  */
+    template <typename...Args>
+    void print(Args...args)
+    {
+        ((std::cout << args << " "), ... , (std::cout << '\n'));
+    }
+};
 
 int main()
 {
   // should produce an empty line
-  print();
+  method_a::print();
+  method_fe::print();
 
-  // should produce:
   // a 3.5 100 hello
-  print('a', 3.5, 100, "hello");
+  method_a::print('a', 3.5, 100, "hello");
+  method_fe::print('a', 3.5, 100, "hello");
 
   // should produce:
   // a b
-  print('a', 'b');
+  method_a::print('a', 'b');
+  method_fe::print('a', 'b');
 
   // should produce:
   // X Y Z
-  print(X{}, "Y", "Z");
+  method_a::print(X{}, "Y", "Z");
+  method_fe::print(X{}, "Y", "Z");
+
 }
